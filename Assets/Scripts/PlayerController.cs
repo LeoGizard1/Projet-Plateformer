3812c15f7280a408e52bc2e1eec5ad11d6671f81
Léo GIZARD <leo.gizard@outlook.fr>
2023-10-03 15:42:42 +0200

    Adding not very satisfying collision with wall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,39 +2,85 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
 
+    private enum CollisionDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+    }
     public bool grounded { get; private set; } = false;
+    private GameObject ground;
 
     private Rigidbody2D _rigidbody2D;
+
+    private Collider2D _collider2D;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _collider2D = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        OffsetCollider();
     }
 
-    private void snapToGround(Transform groundTransform)
+    //offset collider to prevent clipping in the wall/ground.
+    private void OffsetCollider()
     {
-        _rigidbody2D.position = new Vector2(_rigidbody2D.position.x, groundTransform.position.y + 1f);
+        _collider2D.offset = _rigidbody2D.velocity * Time.deltaTime;
     }
     
-    private void OnCollisionEnter2D(Collision2D other)
+    private void SnapToPlatform(Transform groundTransform, CollisionDirection direction)
     {
-        Debug.Log(String.Format("Collision detected between : {0} and {1}",this.name,other.collider.name));
-        var raycastCollider = Physics2D.Raycast(transform.position, Vector2.down, 0.6f).collider;
-        Debug.Log(String.Format("Raycast and collider : {0}  {1}",raycastCollider.name,other.collider.name));
-        if (other.collider == raycastCollider)
+        Debug.Log(String.Format("Snapping to {0} {1}",groundTransform,direction));
+        switch (direction)
         {
+            case CollisionDirection.Down:
+                _rigidbody2D.position = new Vector2(_rigidbody2D.position.x,
+                                                groundTransform.position.y + 1f);
+                break;
+            case CollisionDirection.Up:
+                _rigidbody2D.position = new Vector2(_rigidbody2D.position.x, groundTransform.position.y - 1f); 
+                break;
+            case CollisionDirection.Right:
+                _rigidbody2D.position = new Vector2(groundTransform.position.x - 0.5f - 1f * groundTransform.lossyScale.x/2f, _rigidbody2D.position.y);
+                break;
+
+        }
+    }
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        var downCast = Physics2D.BoxCast(transform.position + Vector3.down, new Vector2(0.8f,0.2f),0f,Vector2.down,0f).collider;
+        var upCast = Physics2D.BoxCast((transform.position + Vector3.up),new Vector2(0.8f,0.2f), 0f, Vector2.up,0f).collider;
+        var leftCast = Physics2D.BoxCast(transform.position + Vector3.left, new Vector2(0.2f,0.8f), 0f, Vector2.left,0f).collider;
+        var rightCast = Physics2D.BoxCast(transform.position + Vector3.right,new Vector2(0.2f,0.8f), 0f,Vector2.right,0f).collider;
+        if (other.collider == downCast)
+        {
+            ground = other.collider.gameObject;
             grounded = true;
-            snapToGround(other.transform);
+            SnapToPlatform(other.transform,CollisionDirection.Down);
+        }
+        else if (other.collider == upCast)
+        {
+            SnapToPlatform(other.transform, CollisionDirection.Up);
+        }
+        else if (other.collider == leftCast)
+        {
+            SnapToPlatform(other.transform, CollisionDirection.Left);
+        }
+        else if (other.collider == rightCast)
+        {
+            SnapToPlatform(other.transform, CollisionDirection.Right);
         }
     }
     
@@ -42,10 +88,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D other)
     {
-        var raycastHit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
-        if (raycastHit.collider == null)
-        {
+        if (other.gameObject == ground)
             grounded = false;
-        }
     }
 }
